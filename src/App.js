77dc58ebe39209aed4ef1209f3a8feebc3000552
timetherewere <mikjosjon@gmail.com>
2020-06-06 13:50:11 -0400

    Implemented basic div CSS for app, header, total profit, and transaction object colors and sizes.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: []
+    }
+
+    // Bind handlers
+    // this.addTransactionHandler.bind(this);
+  }
+
+  /*
+  addTransactionHandler(transaction) {
+    transactions = this.state.transactions.slice();
+    transactions.push(transaction);
+  }
+  */
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Header"><h1>Monero Profit Calculator</h1></div>
+        <div className="TotalProfit"></div>
+        <div className="Transaction"></div>
+        <div className="Transaction"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
