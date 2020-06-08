46e1533170cfe90a818cbcf7d15914a517b9070d
timetherewere <mikjosjon@gmail.com>
2020-06-08 05:01:16 -0400

    Implemented callback functions to main App component so that app's state updates when the Volume field of a Tx is changed
    * still need to implement same functionality for date field
    * still need to update profit table data values

--- src/App.js
+++ src/App.js
@@ -5,16 +5,74 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactions: []
+      transactions: [],
+      investmentData: [],
+      profitData: []
     }
     // Bind handlers
     this.addTransaction = this.addTransaction.bind(this);
+    this.handleVolumeChange = this.handleVolumeChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
   }
 
   addTransaction(){
     let transactions = this.state.transactions.slice();
-    transactions.push(<TransactionContainer />);
-    this.setState({transactions: transactions});
+    let investmentData = this.state.investmentData.slice();
+    let profitData = this.state.profitData.slice();
+    investmentData.push({
+      volume: 0,
+      date: new Date
+    });
+    profitData.push({
+      valueAtPurchase: 0,
+      valueAtPresent: 0,
+      valueChange: 0
+    });
+
+    transactions.push(<TransactionContainer
+      id={transactions.length}
+      investmentData = {investmentData[investmentData.length-1]}
+      profitData = {profitData[profitData.length-1]}
+      handleVolumeChange={this.handleVolumeChange}
+      handleDateChange={this.handleDateChange}
+    />);
+
+    this.setState({
+      transactions: transactions,
+      investmentData: investmentData,
+      profitData: profitData
+    });
+  }
+
+  handleVolumeChange(transactionId, volume) {
+    alert("Txid: " + transactionId + " , Volume: " + volume);
+    let investmentData = this.state.investmentData.slice();
+    let profitData = this.state.profitData.slice();
+
+    investmentData[transactionId] = {
+      volume: volume,
+      date: investmentData[transactionId].date
+    };
+
+    //TEMPORARY - right now this function just needs to run.
+    // Later it will need to calculate actual present and historical
+    // monero values based oncoingecko API queries
+    profitData[transactionId] = {
+      valueAtPurchase: volume,
+      valueAtPresent: volume,
+      valueChange: volume
+    };
+    this.setState((state, props) => ({
+      transactions: state.transactions,
+      investmentData: investmentData,
+      profitData: profitData
+    }));
+    alert("changeHandler running!");
+  }
+
+  handleDateChange(transactionId, date) {
+    // Will implement this function later
+    // Requires coingecko API interaction
   }
 
   render() {
@@ -29,16 +87,48 @@ class App extends React.Component {
   }
 }
 
-function TransactionContainer(props) {
-  return(
-    <div className="Transaction">
-      <div className="EntryArea">
-        <Prompt type="text" name="Volume" size="12" />
-        <Prompt type="date" name="Date" />
+class TransactionContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onVolumeChange = this.onVolumeChange.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
+  }
+
+  onVolumeChange(event) {
+    this.setState({volumeInput: event.target.value});
+    this.props.handleVolumeChange(this.props.id, event.target.value);
+  }
+
+  onDateChange(event) {
+    this.props.handleDateChange(this.props.id, this._dateInput)
+  }
+
+  render() {
+    return(
+      <div className="Transaction">
+        <div className="EntryArea">
+          <Prompt
+            ref={(c) => this.volumeInput = c}
+            type="number"
+            name="Volume"
+            size="12"
+            value="this.state.volumeInput"
+            onKeyUp={this.onVolumeChange}
+            onMouseUp={this.onDateChange}
+          />
+          <Prompt
+            ref={(c) => this._dateInput = c}
+            type="date"
+            name="Date"
+            onKeyUp={this.onDateChange}
+            onMouseUp={this.onDateChange}
+          />
+        </div>
+        <ProfitDataTable data={this.props.profitData} />
       </div>
-      <ProfitDataTable />
-    </div>
-  );
+    );
+  };
 }
 
 class AddTransactionButton extends React.Component {
@@ -54,7 +144,7 @@ class AddTransactionButton extends React.Component {
 function Prompt(props) {
   return(
   <div className="Prompt">
-    {props.name}: <input type={props.type} name={props.name} size={props.size} />
+    {props.name}: <input type={props.type} name={props.name} size={props.size} onKeyUp={props.onKeyUp} onMouseUp={props.onMouseUp} />
   </div>
   );
 }
@@ -74,7 +164,7 @@ class ProfitDataTable extends React.Component {
           <td>100.00</td><td>100.00</td>
         </tr>
         <tr>
-          <td>100.00</td><td>100.00</td>
+          <td>Profit/Loss: </td><td>100.00</td>
         </tr>
       </table>
     );
